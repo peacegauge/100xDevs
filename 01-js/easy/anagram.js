6a0aef0a5ee143ce810df94533c6498fbde034bf
ezraorbit <emakankwasa@alu.ucam.edu>
2024-09-26 09:50:09 +0200

    Add exercises for week 1

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -4,8 +4,39 @@
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
 
-function isAnagram(str1, str2) {
 
+// sort letters in descending order
+
+
+function sortString(str) {
+  str = str.toLowerCase()
+  newStr = Array.from(str);
+
+  for (let i = 0; i < newStr.length; i++) {
+
+    for (let j = newStr.length - 1; j > 0; j--) {
+      if (newStr[i].charCodeAt(0) > newStr[j].charCodeAt(0)) {
+        temp = newStr[i];
+        newStr[i] = newStr[j];
+        newStr[j] = temp;
+      }
+
+      if (j == newStr.length) {
+        break;
+      }
+
+    }
+  }
+  return newStr.toString();
+}
+
+function isAnagram(str1, str2) {
+  
+  if (sortString(str1) === sortString(str2)) {
+    return true
+  } else {
+    return false
+  }
 }
 
 module.exports = isAnagram;
--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,6 +6,15 @@
 */
 
 function findLargestElement(numbers) {
+    let max = 0;
+    
+    for(let i = 0; i < numbers.length; i++){
+        if(numbers[i] > max){
+            max = numbers[i];
+        }
+    }
+
+    return max;
     
 }
 
